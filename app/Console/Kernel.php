498d7200962edf503bfdf8cb24712893499972f9
Asmaa.Mohamed <asm2kh@gmail.com>
2022-04-02 20:10:48 +0200

    last commi-m

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('status:finish')->hourly();
+        $schedule->command('status:finish')->everyMinute();
     }
 
     /**
